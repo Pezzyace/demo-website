d4696fcce37cc85fcc8ae4e280fb7d30691928fc
Akaehomen peace <peacedavid049@gmail.com>
2023-12-16 17:21:20 +0100

    commit

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,10 @@
+import Logo from "./assets/react-svgrepo-com.svg";
+
 export default function Header() {
   return(
     <header>
         <nav className="nav">
-          <img src="assets/react-svgrepo-com.svg" alt="React Logo" />
+          <img src={Logo} alt="React Logo" />
           <ul className="nav-items">
             <li>Home</li>
             <li>Classes</li>
